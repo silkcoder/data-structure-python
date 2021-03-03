b43bbb7ba3dadf23a226556635af28777c4c1698
silkcoder <79930087+silkcoder@users.noreply.github.com>
2021-03-03 22:15:01 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,20 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop()
+
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+print(queue.dequeue())
+
